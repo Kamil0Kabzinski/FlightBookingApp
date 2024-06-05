9cd13c4b8a052d0c74d1427ad6dee1013ca28340
Kamil0Kabzinski <kamkabz10@gmail.com>
2024-06-05 16:13:34 +0200

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'ionic.flightbookingapp',
+  appName: 'flightbookingapp',
+  webDir: 'www'
+};
+
+export default config;
